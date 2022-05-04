b33ca95798ff4fd3980b92b6e7db38e3fab0999f
Xuan-Manh <manhnx0204@gmail.com>
2022-05-04 09:15:03 +0700

    add hello 4

---
+++ hello4.java
@@ -0,0 +1,11 @@
+package java_1;
+
+public class hello4 {
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+		System.out.print("Nguyen Xuan Manh");
+		System.out.print("Xuan Manh");
+		System.out.print("Manh");
+		
+	}
+}
