8ff3a5d887dc82ed77f13861f868fc5e06ce96dc
Xuan-Manh <manhnx0204@gmail.com>
2022-05-04 08:46:53 +0700

    add them hello2.java

--- hello3.java
+++ hello3.java
@@ -1,6 +1,6 @@
 package java_1;
 
-public class hello2 {
+public class hello3 {
 	public static void main(String[] args) {
 		System.out.print("Hello World");
 		System.out.print("Nguyen Xuan Manh");
